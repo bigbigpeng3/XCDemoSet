5d7c12c134a843d83004ac49ceacf6c1ad515815
xiaohao9509 <chenxiang278>
2017-05-02 10:37:42 +0800

    !!!

--- app/src/main/java/com/pengzhangdemo/com/myapplication/MainActivity.java
+++ app/src/main/java/com/pengzhangdemo/com/myapplication/MainActivity.java
@@ -50,6 +50,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnVPinBottomSheet = (Button) findViewById(R.id.btn_vp_in_bottom_sheet);
         btnVPinBottomSheet.setOnClickListener(this);
 
+        findViewById(R.id.btn_normal_fragment).setOnClickListener(this);
 //        RoundCornerProgressBar progress1 = (RoundCornerProgressBar) findViewById(R.id.progress_1);
 //        progress1.setProgressColor(Color.parseColor("#ed3b27"));
 //        progress1.setProgressBackgroundColor(Color.parseColor("#808080"));
@@ -86,6 +87,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(new Intent(this, BottomSheetViewPageActivity.class));
         }else if (v == btnVPinBottomSheet){
             startActivity(new Intent(this, ViewPageInBottomSheetDialogActivity.class));
+        }else if (v.getId() == R.id.btn_normal_fragment){
+            startActivity(new Intent(this, FragmentActivity.class));
         }
 
     }
